f0adc165286a9d0c886f72192d06d4981d27b725
MarihTavares <marina.ar1308@gmail.com>
2026-06-01 20:20:19 -0300

    navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,34 @@ import styles from './Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <div className={styles.card}>
+    <div>
+      <div className={styles.background}>
+        <div className={styles.glow}/>       
+          <nav className={styles.nav}>
+            <div className={styles.div_structure}>
+              <a className={styles.image} href="#">
+                <img src="/src/assets/icon-carbontracker.svg" alt="Logo" />
+                <span>CarbonTracker Space</span>
+              </a>
+
+                <div className={styles.menu}>
+                  <a href="#">Quem somos nós</a>
+                  <a href="#">Funcionalidades</a>
+                  <a href="#">Público-Alvo</a>
+                </div>
+            </div>
+
+            <div className={styles.button}>
+              <button type="button">Acesse a Plataforma</button>
+            </div>
+          </nav>
+      </div>
+
+      {/* <div className={styles.card}>
         <h1 className={styles.title}>Landing Page em desenvolvimento</h1>
         <p className={styles.subtitle}>Desenvolva Aqui</p>
         <Link to="/login" className={styles.btn}>Entrar</Link>
-      </div>
+      </div> */}
     </div>
   )
 }
